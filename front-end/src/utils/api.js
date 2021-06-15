a587090ad1fa3f799f897b134b053b727ffa1f1a
James Hanna <James.Hanna87@gmail.com>
2021-06-15 16:50:31 -0600

    na

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://reservation-app-backend.vercel.app/";
+  process.env.REACT_APP_API_BASE_URL || "https://reservation-app-backend.vercel.app";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
